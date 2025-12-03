f46105d12f27c73eaaa25d752d25e791603cd227
SofiSoler <s.soler@alumno.um.edu.ar>
2025-12-03 11:53:10 -0300

    Configuracion de perfiles

---
+++ eventosBackend/src/main/java/com/um/eventosbackend/config/CatedraProperties.java
@@ -0,0 +1,28 @@
+package com.um.eventosbackend.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "catedra")
+public class CatedraProperties {
+
+    private String baseUrl;
+    private String apiToken;
+
+    // getters y setters
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
+    public void setApiToken(String apiToken) {
+        this.apiToken = apiToken;
+    }
+}
