be958a9fd5cf2c881a050be811f26dfcd2c6ac47
SofiSoler <s.soler@alumno.um.edu.ar>
2025-12-10 11:33:24 -0300

    Creado servicio de modificacion

--- eventosBackend/src/main/java/com/um/eventosbackend/repository/EventoLocalRepository.java
+++ eventosBackend/src/main/java/com/um/eventosbackend/repository/EventoLocalRepository.java
@@ -4,5 +4,10 @@ import com.um.eventosbackend.domain.EventoLocal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface EventoLocalRepository extends JpaRepository<EventoLocal, Long> {}
+public interface EventoLocalRepository extends JpaRepository<EventoLocal, Long> {
+
+    Optional<EventoLocal> findByIdCatedra(Long idCatedra);
+}
